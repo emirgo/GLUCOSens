46bf5f8c25b082d3de8b551683f69d23d6aad7fe
Emir Gocturk <emirgoppa@gmail.com>
2021-10-30 09:35:15 +0200

    Real time plotting

--- Client/main.py
+++ Client/main.py
@@ -10,6 +10,13 @@ import serial
 import serial.tools.list_ports
 import matplotlib
 from serial.tools.list_ports_windows import comports
+import serial
+import time
+import csv
+import matplotlib
+matplotlib.use("tkAgg")
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 #to be used on our canvas
@@ -37,6 +44,36 @@ def realTimePlotting():
     comPort = cb.get().split(" ", 1)
     print(comPort[0])
     ser = serial.Serial(comPort[0])
+    ser.flushInput()
+
+    plot_window = 20
+    y_var = np.array(np.zeros([plot_window]))
+
+    plt.ion()
+    fig, ax = plt.subplots()
+    line, = ax.plot(y_var)
+
+    while True:
+        try:
+            ser_bytes = ser.readline()
+            try:
+                decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+                print(decoded_bytes)
+            except:
+                continue
+            with open("test_data.csv","a") as f:
+                writer = csv.writer(f,delimiter=",")
+                writer.writerow([time.time(),decoded_bytes])
+            y_var = np.append(y_var,decoded_bytes)
+            y_var = y_var[1:plot_window+1]
+            line.set_ydata(y_var)
+            ax.relim()
+            ax.autoscale_view()
+            fig.canvas.draw()
+            fig.canvas.flush_events()
+        except:
+            print("Keyboard Interrupt")
+            break
 
 # --- functions ---
 
